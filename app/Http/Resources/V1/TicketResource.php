c4df2f241b7f8de523d74c73e444c27e9a7745b2
Utrodus <contact.utrodus@gmail.com>
2026-02-22 21:36:03 +0700

    feat: implement TicketResource for ticket data transformation and update TicketController to use it

---
+++ app/Http/Resources/V1/TicketResource.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use App\Http\Resources\V1\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'ticket',
+            'id' => $this->id,
+            'attributes' => [
+                'title' => $this->title,
+                'description' => $this->when(
+                    $request->routeIs('tickets.show'),
+                    $this->description,
+                ),
+                'status' => $this->status,
+                'createdAt' => $this->created_at,
+                'updatedAt' => $this->updated_at,
+            ],
+            'relationships' => [
+                'author' => [
+                    'data' => [
+                        'type' => 'user',
+                        'id' => $this->user_id,
+                    ],
+                    'links' => [
+                        'self' => 'todo'
+                    ]
+                ]
+            ],
+            'includes' => [
+                new UserResource($this->user)
+            ],
+            'links' => [
+                'self' => route('tickets.show', ['ticket' => $this->id])
+            ]
+        ];
+    }
+}
