2b9acb9dd0dc10eb75ec1fd1fe4b5d0fabf380ed
Utrodus <contact.utrodus@gmail.com>
2026-02-22 21:34:57 +0700

    feat: implement AuthController with login, dummy register, and logout methods; add LoginUserRequest for validation

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post("/login", [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 
 Route::get('/user', function (Request $request) {
