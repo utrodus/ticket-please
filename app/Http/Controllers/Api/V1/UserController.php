8c457d705ee0b1652927529a663df23920911f82
Utrodus <contact.utrodus@gmail.com>
2026-02-22 23:37:54 +0700

    feat: update UserController to include tickets relationship in index and show methods

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\StoreUserRequest;
 use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+class UserController extends ApiController
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
+        if ($this->include('tickets')) {
+            return UserResource::collection(User::with('tickets')->paginate());
+        }
         return UserResource::collection(User::paginate(10));
     }
 
@@ -31,6 +33,9 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
+        if ($this->include('tickets')) {
+            return new UserResource($user->load('tickets'));
+        }
         return new UserResource($user);
     }
 
