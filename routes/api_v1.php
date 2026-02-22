9eb0c7571dbef3bf2fbad00b78407cdba107403b
Utrodus <contact.utrodus@gmail.com>
2026-02-22 21:36:13 +0700

    feat: add authentication middleware to tickets and users API resources

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('users', UserController::class);
