f67ab7fc0b1744d05fd37c310bf9d80620f6f080
Utrodus <contact.utrodus@gmail.com>
2026-02-22 21:32:12 +0700

    feat: enhance ApiResponses trait to include optional data in ok and success methods

--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -4,11 +4,19 @@ namespace App\Traits;
 
 trait ApiResponses
 {
-    protected function ok($message)
+    protected function ok($message, $data = null)
     {
-        return $this->success($message, 200);
+        return $this->success($message, $data, 200);
     }
-    protected function success($message, $statusCode = 200)
+    protected function success($message, $data, $statusCode = 200)
+    {
+        return response()->json([
+            'data' => $data,
+            'message' => $message,
+            'status' => $statusCode,
+        ], $statusCode);
+    }
+    protected function error($message, $statusCode)
     {
         return response()->json([
             'message' => $message,
