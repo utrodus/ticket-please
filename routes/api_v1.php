1aa619c3e0526734d8eca5444ae42218456edfbc
Utrodus <contact.utrodus@gmail.com>
2026-02-24 21:39:01 +0700

    feat: update routes to replace UserController with AuthorController and add AuthorTicketController

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
-use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\AuthorController;
+use App\Http\Controllers\Api\V1\AuthorTicketController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
-Route::middleware('auth:sanctum')->apiResource('users', UserController::class);
+Route::middleware('auth:sanctum')->apiResource('authors', AuthorController::class);
+Route::middleware('auth:sanctum')->apiResource('authors.tickets', AuthorTicketController::class);
