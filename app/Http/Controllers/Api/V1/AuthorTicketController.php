e1736909efc11cc34d90e771b16611aa4ef20e6d
Utrodus <contact.utrodus@gmail.com>
2026-02-25 22:50:54 +0700

    feat: implement delete ticket by author id

--- app/Http/Controllers/Api/V1/AuthorTicketController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
-class AuthorTicketController extends Controller
+class AuthorTicketController extends ApiController
 {
     public function index($author_id, TicketFilter $filters)
     {
@@ -26,5 +26,25 @@ class AuthorTicketController extends Controller
         ];
 
         return new TicketResource(Ticket::create($model));
+    }
+
+    /**
+     * 
+     * Remove the specified resource from storage.
+     */
+    public function destroy($authorId, $ticketId)
+    {
+        try {
+            $ticket = Ticket::findOrFail($ticketId);
+
+            if ($ticket->user_id == $authorId) {
+                $ticket->delete();
+                return $this->ok('Ticket deleted successfully');
+            } else {
+                return $this->error('Ticket cannot found', 404);
+            }
+        } catch (ModelNotFoundException $e) {
+            return $this->error('Ticket cannot found', 404);
+        }
     }
 }
