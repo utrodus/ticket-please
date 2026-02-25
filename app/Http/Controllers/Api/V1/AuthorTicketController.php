6fbbdb7ce6d51a148598587f76767f8a3bb3b67b
Utrodus <contact.utrodus@gmail.com>
2026-02-25 21:27:55 +0700

    feat: add store method to AuthorTicketController for ticket creation

--- app/Http/Controllers/Api/V1/AuthorTicketController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
@@ -13,5 +14,17 @@ class AuthorTicketController extends Controller
     public function index($author_id, TicketFilter $filters)
     {
         return TicketResource::collection(Ticket::where('user_id', $author_id)->filter($filters)->paginate());
+    }
+
+    public function store($authorId, StoreTicketRequest $request)
+    {
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $authorId,
+        ];
+
+        return new TicketResource(Ticket::create($model));
     }
 }
