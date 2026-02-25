65af9df11c616005658787c12aca693ed32a084a
Utrodus <contact.utrodus@gmail.com>
2026-02-25 21:26:24 +0700

    feat: integrate ApiResponses trait into ApiController for enhanced response handling

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Traits\ApiResponses;
 
 class ApiController extends Controller
 {
+    use ApiResponses;
     public function include(string $relationship): bool
     {
         $param = request()->query('include');
