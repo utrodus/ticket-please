5d656b982f2858446168d2a4f97169dd98552f70
Utrodus <contact.utrodus@gmail.com>
2026-02-25 22:33:56 +0700

    feat: update show and destroy methods in TicketController to handle ticket retrieval and deletion by ID

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -48,13 +48,17 @@ class TicketController extends ApiController
     /**
      * Display the specified resource.
      */
-    public function show(Ticket $ticket)
+    public function show($ticketId)
     {
-        if ($this->include('author')) {
-            return new TicketResource($ticket->load('user'));
+        try {
+            $ticket = Ticket::findOrFail($ticketId);
+            if ($this->include('author')) {
+                return new TicketResource($ticket->load('author'));
+            }
+            return new TicketResource($ticket);
+        } catch (ModelNotFoundException $e) {
+            return $this->error('Ticket cannot found', 404);
         }
-
-        return new TicketResource($ticket);
     }
 
 
@@ -69,8 +73,14 @@ class TicketController extends ApiController
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Ticket $ticket)
+    public function destroy($ticketId)
     {
-        //
+        try {
+            $ticket = Ticket::findOrFail($ticketId);
+            $ticket->delete();
+            return $this->ok('Ticket deleted successfully');
+        } catch (ModelNotFoundException $e) {
+            return $this->error('Ticket cannot found', 404);
+        }
     }
 }
